217be5284b8a25f0d497eb9a18b1e8f98ccec6c4
Betta_Fish <Betta_Fish@LAPTOP-576M8DKG>
2025-06-15 00:03:10 +0800

    添加项目文件。

---
+++ OpenInTerminal/ExtensionEntrypoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace OpenInTerminal
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class ExtensionEntrypoint : Extension
+    {
+        /// <inheritdoc/>
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            Metadata = new(
+                    id: "OpenInTerminal.d552e077-bf83-45a2-a547-d4deb485a3ed",
+                    version: this.ExtensionAssemblyVersion,
+                    publisherName: "Publisher name",
+                    displayName: "OpenInTerminal",
+                    description: "Extension description"),
+        };
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+
+            // You can configure dependency injection here by adding services to the serviceCollection.
+        }
+    }
+}
---
+++ OpenInTerminal/OpenInTerminalCommand.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Shell;
+using System.Diagnostics;
+using System.Windows.Documents;
+
+namespace OpenInTerminal
+{
+    /// <summary>
+    /// Command1 handler.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class OpenInTerminalCommand : Command
+    {
+        private readonly TraceSource logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenInTerminalCommand"/> class.
+        /// </summary>
+        /// <param name="traceSource">Trace source instance to utilize.</param>
+        public OpenInTerminalCommand(TraceSource traceSource)
+        {
+            // This optional TraceSource can be used for logging in the command. You can use dependency injection to access
+            // other services here as well.
+            this.logger = Requires.NotNull(traceSource, nameof(traceSource));
+        }
+
+        /// <inheritdoc />
+        public override CommandConfiguration CommandConfiguration => new("%OpenInTerminal.OpenInTerminalCommand.DisplayName%")
+        {
+            // Use this object initializer to set optional parameters for the command. The required parameter,
+            // displayName, is set above. DisplayName is localized and references an entry in .vsextension\string-resources.json.
+            Icon = new(ImageMoniker.KnownValues.OpenFileDialog, IconSettings.IconAndText),
+            Placements =
+            [
+                // File in project context menu
+                CommandPlacement.VsctParent(new Guid("{d309f791-903f-11d0-9efc-00a0c911004f}"), id: 0x0209, priority: 0x1600),
+
+                // Project context menu
+                CommandPlacement.VsctParent(new Guid("{d309f791-903f-11d0-9efc-00a0c911004f}"), id: 0x0266, priority: 0x1600), 
+
+                // Folder context menu
+                CommandPlacement.VsctParent(new Guid("{d309f791-903f-11d0-9efc-00a0c911004f}"), id: 0x02A3, priority: 0x1600),
+            ],
+        };
+
+        /// <inheritdoc />
+        public override Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            // Use InitializeAsync for any one-time setup or initialization.
+            return base.InitializeAsync(cancellationToken);
+        }
+
+        /// <inheritdoc />
+        public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+        {
+            string dir = "";
+            try
+            {
+                dir = Path.GetDirectoryName(context.GetSelectedPathAsync(cancellationToken).Result.LocalPath);
+            }
+            catch (Exception)
+            {
+            }
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "wt.exe",
+                Arguments = $"wt -d \"{dir}\"",
+                UseShellExecute = true,
+                CreateNoWindow = true  // 不显示命令窗口
+            };
+            Process.Start(startInfo);
+        }
+    }
+}
